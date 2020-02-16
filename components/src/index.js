a201ae300f564a9d47a304660741d986b5c16c59
Arpit Gupta <arpit.gupta@diconium.com>
2020-02-16 19:05:15 +0100

    Component | Showing Custom Children

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,39 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today ar 4:45PM"
-        comment="It was a nice post !!"
-        avatar={Faker.image.avatar()}
-      />
-      <ApprovalCard />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today ar 2:00PM"
-        comment="I loved your blog."
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday ar 0:45PM"
-        comment="Wow this was eye catching :)"
-        avatar={Faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning !</h4>
+          Are you sure you understand?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today ar 4:45PM"
+          comment="It was a nice post !!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today ar 2:00PM"
+          comment="I loved your blog."
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday ar 0:45PM"
+          comment="Wow this was eye catching :)"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

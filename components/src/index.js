2878d165e1fda76f30ae8f1542113e1d20e8ffa9
Arpit Gupta <arpit.gupta@diconium.com>
2020-02-16 16:26:37 +0100

    Component | Created HTML for Comments | Added Semantic Style

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return <div>Hi There !!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metaData">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice blog post !!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector("#root"));

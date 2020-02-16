a2229d5077f370df9e66fa6efcb3dc3e0796d95a
Arpit Gupta <arpit.gupta@diconium.com>
2020-02-16 17:28:43 +0100

    Component | Using Props to display data

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom";
 import CommentDetail from "./CommentDetail";
+import Faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today ar 4:45PM"
+        comment="It was a nice post !!"
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today ar 2:00PM"
+        comment="I loved your blog."
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday ar 0:45PM"
+        comment="Wow this was eye catching :)"
+        avatar={Faker.image.avatar()}
+      />
     </div>
   );
 };
